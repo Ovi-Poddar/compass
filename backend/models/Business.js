2079e648a870ccbce8c79a8974d262d88552e03a
Prantik Paul <1705071@ugrad.cse.buet.ac.bd>
2022-07-08 23:44:27 +0600

    Added schemas

---
+++ backend/models/Business.js
@@ -0,0 +1,89 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const businessSchema = new Schema({
+    business_name: {
+        type: String,
+        required: true,
+    }
+    ,
+    owner_id: {  // foreign key
+        type : mongoose.Schema.Types.ObjectID,
+        ref : 'User',
+        required: true,
+    },
+
+    contact_no: {
+        type: String,
+    },
+
+    address: {
+        type : String,
+        required: true,
+    },
+
+    area: {
+        type: String,
+        required: true,
+    },
+
+    city: {
+        type: String,
+        required: true,
+    },
+
+    category: {
+        type: [String],
+        required: true,
+    },
+
+    tags: {
+        type: [String],
+        required: true,
+    },
+    
+    is_open: {
+        type: Boolean,
+        required: true,
+        default : False,
+    },
+
+    review_count: {
+        type: Number,
+        default: 0,
+    },
+    
+    asked_query_count: {
+        type: Number,
+        default: 0,
+    },
+
+    answered_query_count: {
+        type: Number,
+        default: 0,
+    },
+
+    post_count: {
+        type: Number,
+        default: 0,
+    },
+
+    offer_count: {
+        type: Number,
+        default: 0,
+    },
+
+    average_star_count: {
+        //type : Number,
+        type: Decimal128,  //dont know if works found in stackoverflow
+        default: 0,
+    },
+
+    opening_hours: {
+        type: [String],
+    },
+});
+
+const Business = mongoose.model("Business", businessSchema);
+
+module.exports = Business;
