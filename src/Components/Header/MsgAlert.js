afb1fe6e8f5a4bd93de04dad7981ee03add1d484
Ovi Poddar <ovi.poddar2@gmail.com>
2022-07-09 17:25:25 +0600

    Alert Message File Added

---
+++ src/Components/Header/MsgAlert.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import WarningAmberIcon from '@mui/icons-material/WarningAmber';
+import InfoIcon from '@mui/icons-material/Info';
+
+export default function MsgAlert() {
+  return (
+    <div className="alert alert-success alert-dismissible fade show container" role="alert">
+      <strong> <CheckCircleOutlineIcon/>  Holy guacamole!</strong>
+      <button
+        type="button"
+        className="close"
+        data-dismiss="alert"
+        aria-label="Close"
+      >
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
+}
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import MsgAlert from '../Header/MsgAlert'
 import HomeCarousel from './HomeCarousel'
 
 export default function Home() {
   return (
     <>
+        <MsgAlert/>
         <HomeCarousel/>
     </>
   )
