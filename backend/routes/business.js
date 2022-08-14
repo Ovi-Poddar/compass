12a8be59a0c71053c6861a8672f015495178acac
tanvir64 <traihan3130@gmail.com>
2022-08-14 18:01:42 +0600

    business api completed

--- backend/routes/business.js
+++ backend/routes/business.js
@@ -6,14 +6,12 @@ const multer = require("multer");
 // const upload = multer({ dest: 'uploads/' })
 var fetchUser = require("../middleware/fetchUser");
 
-const {
-  addImage, addMultipleImages,
- } = require('../imageController');
+const { addImage, addMultipleImages } = require("../imageController");
 
 // Setting up multer as a middleware to grab photo uploads
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage }).single('profile_image');
-const uploads = multer({ storage: storage }).array('images[]', 10);
+const upload = multer({ storage: storage }).single("profile_image");
+const uploads = multer({ storage: storage }).array("images[]", 10);
 // const uploads = multer({ dest: 'uploads/' })
 
 // ROUTE 1: Get All the Businesses of this user using: GET "/api/business/getownbusinesses". Login required
@@ -136,33 +134,39 @@ router.get("/getowner/:business_id", fetchUser, async (req, res) => {
 });
 
 //Route 7 : upload photos to a business using: POST "/api/business/uploadphotos". Login required
-router.post('/uploadphotos', uploads, fetchUser, addMultipleImages, async (req, res, next) => {
-  try {
-    const { business_id } = req.body;
-    const downloadURLs =  req.downloadURLs;
-    console.log("downloadURLs", downloadURLs);
-    const business = await Business.findOne({ _id: business_id });
-    // console.log(Object.keys(business.images));  
-    /**
-     * You can't just push the downloadURLs to the images array,
-     * because the array is an array of objects, not an array of strings.
-     * So you need to convert the string array to an array of objects.
-     * then push the objects to the images array. 
-     * here, key is the number of the image, and value is the downloadURL
-     */
-    let length = Object.keys(business.images).length;
-    downloadURLs.forEach(url => {
-      business.images[length] = url;
-      length++;
-    });
-    //save the business with the new images array
-    const savedBusiness = await business.save();
-    res.json({ success: true, images: savedBusiness.images });
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send("Internal Server Error");
+router.post(
+  "/uploadphotos",
+  uploads,
+  fetchUser,
+  addMultipleImages,
+  async (req, res, next) => {
+    try {
+      const { business_id } = req.body;
+      const downloadURLs = req.downloadURLs;
+      console.log("downloadURLs", downloadURLs);
+      const business = await Business.findOne({ _id: business_id });
+      // console.log(Object.keys(business.images));
+      /**
+       * You can't just push the downloadURLs to the images array,
+       * because the array is an array of objects, not an array of strings.
+       * So you need to convert the string array to an array of objects.
+       * then push the objects to the images array.
+       * here, key is the number of the image, and value is the downloadURL
+       */
+      let length = Object.keys(business.images).length;
+      downloadURLs.forEach((url) => {
+        business.images[length] = url;
+        length++;
+      });
+      //save the business with the new images array
+      const savedBusiness = await business.save();
+      res.json({ success: true, images: savedBusiness.images });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
   }
-});
+);
 
 //Route 8: Get all photos of the business using: GET "/api/business/getphotos/:business_id". Login required
 router.get("/getphotos/:business_id", async (req, res) => {
@@ -183,6 +187,84 @@ router.get("/getfullmenu/:business_id", async (req, res) => {
   try {
     const business = await Business.findById(req.params.business_id);
     res.json(business);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// ROUTE 10: Add new offers the Business using: POST "/api/business/addoffers". Login required
+router.post("/addoffers", fetchUser, async (req, res) => {
+  try {
+    const { business_id } = req.body;
+    const business = await Business.findById(business_id);
+    business.offers.push(req.body.offer);
+    const savedBusiness = await business.save();
+    res.json({ success: true, business: savedBusiness });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// ROUTE 11: Get all offers of the Business using: GET "/api/business/getoffers/:business_id". Login required
+router.get("/getoffers/:business_id", async (req, res) => {
+  try {
+    const { business_id } = req.params;
+    const business = await Business.findById(business_id);
+    res.json(business.offers);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// ROUTE 12: Add updates to the Business using: GET "/api/business/addupdates". Login required
+router.post("/addupdates", fetchUser, async (req, res) => {
+  try {
+    const { business_id } = req.body;
+    const business = await Business.findById(business_id);
+    business.updates.push(req.body.update);
+    const savedBusiness = await business.save();
+    res.json({ success: true, business: savedBusiness });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// ROUTE 13: Get all updates of the Business using: GET "/api/business/getupdates/:business_id". Login required
+router.get("/getupdates/:business_id", async (req, res) => {
+  try {
+    const { business_id } = req.params;
+    const business = await Business.findById(business_id);
+    res.json(business.updates);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// ROUTE 14: Add services of the Business using: GET "/api/business/addservices". Login required
+router.post("/addservices", fetchUser, async (req, res) => {
+  try {
+    const { business_id } = req.body;
+    const business = await Business.findById(business_id);
+    business.services.push(req.body.service);
+    const savedBusiness = await business.save();
+    res.json({ success: true, business: savedBusiness });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// ROUTE 15: Get all services of the Business using: GET "/api/business/getservices/:business_id". Login required
+router.get("/getservices/:business_id", async (req, res) => {
+  try {
+    const { business_id } = req.params;
+    const business = await Business.findById(business_id);
+    res.json(business.services);
   } catch (error) {
     console.error(error.message);
     res.status(500).send("Internal Server Error");
