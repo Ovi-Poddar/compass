083cb3669113cc80cad24db202a93b888e6df6dc
Prantik Paul <1705071@ugrad.cse.buet.ac.bd>
2022-08-22 00:56:40 +0600

    errors fixed

--- src/Components/Business/pages/ShortDetails/ShortDetails.js
+++ src/Components/Business/pages/ShortDetails/ShortDetails.js
@@ -50,7 +50,7 @@ function ShortDetails(props) {
           <div class="middle-container d-flex justify-content-between align-items-center mt-2 p-1">
             <div class="icon-div p-2">
               <div class="round-div">
-              <i class="fa-solid fa-cloud icon"></i>
+                <i class="fa-solid fa-cloud icon"></i>
               </div>
             </div>
             <div class="d-flex flex-column text-right mr-2">
@@ -61,7 +61,7 @@ function ShortDetails(props) {
           <div class="middle-container d-flex justify-content-between align-items-center mt-2 p-1">
             <div class="icon-div p-2">
               <div class="round-div">
-              <i class="fa-solid fa-mobile icon"></i>
+                <i class="fa-solid fa-mobile icon"></i>
               </div>
             </div>
             <div class="d-flex flex-column text-right mr-2">
@@ -72,7 +72,7 @@ function ShortDetails(props) {
           <div class="middle-container d-flex justify-content-between align-items-center mt-2 p-1">
             <div class="icon-div p-2">
               <div class="round-div">
-              <i class="fa-solid fa-message icon"></i>
+                <i class="fa-solid fa-message icon"></i>
               </div>
             </div>
             <div class="d-flex flex-column text-right mr-2">
@@ -83,32 +83,15 @@ function ShortDetails(props) {
           <div class="middle-container d-flex justify-content-between align-items-center mt-2 p-1">
             <div class="icon-div p-2">
               <div class="round-div">
-              <i class="fa-solid fa-location-crosshairs icon"></i>
+                <i class="fa-solid fa-location-crosshairs icon"></i>
               </div>
             </div>
             <div class="d-flex flex-column text-right mr-2">
-              <span class="content">{business.address},{business.city},{business.district}</span>
+              <span class="content">
+                {business.address},{business.city},{business.district}
+              </span>
             </div>
           </div>
-
-          
-
-          {/* <Card.Title>{business.business_name}</Card.Title>
-                <Card.Text>
-                    {business.about}
-                </Card.Text>
-                <Card.Text>
-                    {business.contact_no}
-                </Card.Text>
-                <Card.Text>
-                    {business.address}
-                </Card.Text>
-                <Card.Text>
-                    {business.average_star_count}
-                </Card.Text>
-                <Card.Text>
-                    {business.email}
-                </Card.Text> */}
         </Card.Body>
       </Card>
     </>
