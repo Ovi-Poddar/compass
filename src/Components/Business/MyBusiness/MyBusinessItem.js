c4eea7b4c0a148ce09be22a5b0ee2edef9354c33
Ovi Poddar <ovi.poddar2@gmail.com>
2022-08-29 10:19:20 +0600

    My Business Items UI updated

--- src/Components/Business/MyBusiness/MyBusinessItem.js
+++ src/Components/Business/MyBusiness/MyBusinessItem.js
@@ -8,6 +8,8 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
 
+import { Rating } from "@mui/material";
+
 const MyBusinessItem = (props) => {
   return (
     <div className="col-4">
@@ -19,15 +21,19 @@ const MyBusinessItem = (props) => {
           style={{ height: "200px", width: "100%" }}
           src={props.business.profile_image}
         /> </div>
-        <Card.Body style={{height:"200px"}}>
-          <Card.Title className="text-success fw-bold">
+        <Card.Body 
+        // style={{height:"220px"}}
+        >
+          <Card.Title className="text-success fw-bold d-flex justify-content-between">
             <span class="badge bg-success">{props.business.business_name}</span>
+            <Rating value={Math.round(props.business.average_star_count)} readOnly />
           </Card.Title>
-          <Card.Text>
-           {props.business.about.substring(0, 25)} {props.business.about.length >= 25 && '...'}
-          </Card.Text>
-          <Link to={`/business/${props.business._id}`}>
-            <Button variant="warning">Visit</Button>
+          {/* <Card.Text>
+           {props.business.about.substring(0, 100)} {props.business.about.length >= 25 && '...'}
+          </Card.Text> */}
+          <Link to={`/business/${props.business._id}`} className="d-flex justify-content-center"
+          style={{textDecoration:"none"}}>
+            <Button variant="warning" className="mt-2">Visit</Button>
           </Link>
         </Card.Body>
       </Card>
